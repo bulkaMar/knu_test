44b655847accc9f1f750f14f4a2a417d7037f047
Маріна Булах <marinabulah@MacBook-Pro-Marina.local>
2024-05-28 21:01:39 +0300

    add folder knu_test

---
+++ knu_test/views.py
@@ -0,0 +1,8 @@
+# from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def homepage(request):
+    # return HttpResponse("Hello World! I'm Home.")
+    return render(request, 'index.html')
+
